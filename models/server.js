dd5472d2f8442822d8d0a6bfc9903967a1ad8f93
Jordin Bryant <jordintaylor095@gmail.com>
2022-09-01 08:54:24 -0400

    commit

---
+++ models/server.js
@@ -0,0 +1,44 @@
+// REQUIRE DEPENDENCIES
+const express = require('express');
+const drinks = require("./drinks.js")
+const food = require("./food.js")
+// INITIALIZE EXPRESS APP
+const app = express();
+const port = 3000;
+
+
+// DEFINE OUR ROUTES
+
+app.get("/drinks/", (req, res)=>{
+  res.render("drinks_index.ejs", {
+    allDrinks: drinks,
+  })
+})
+app.get("/food/", (req, res)=>{
+  res.render("food_index.ejs", {
+    allFood: food,
+  })
+});
+
+app.get("/drinks/:indexOfDrinksArray", (req, res)=>{
+  res.render('drinks_show.ejs', {
+    drink: drinks[req.params.indexOfDrinksArray],
+  })
+});
+
+app.get("/food/:indexOfFoodArray", (req, res)=>{
+  res.render('food_show.ejs', {
+    food: food[req.params.indexOfFoodArray],
+  })
+});
+
+
+
+
+
+
+// TELL OUR APP TO LISTEN ON PORT...
+app.listen(port, () => {
+  console.log(`listening on port `, port)
+});
+///
